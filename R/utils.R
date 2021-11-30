3dbdb5a6dab534f7c76f894756919e8a1fd598ba
Nick Williams <ntwilliams.personal@gmail.com>
2021-11-30 14:14:01 -0800

    function to compute K/H

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,11 @@
 G <- function(var, g, level) {
     as.numeric(var == level) / g
 }
+
+# G should be a matrix with rows for subjects and columns for time point
+K <- function(G, l, u) {
+    apply(G[, l:u], 1, prod)
+}
+
+# Same as K, but G is the matrix for G_M instead of G_A' or G_A*
+H <- K
